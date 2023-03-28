688bda93a5170fd8890cee1cd40926cb08c2926c
DiegoFusaro <diefusaro@gmail.com>
2023-03-28 11:55:17 -0300

    code: updated css and react components to optimizate the page view

--- components/work/WorkPage.jsx
+++ components/work/WorkPage.jsx
@@ -1,6 +1,9 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { FaReact, FaSass } from 'react-icons/fa'
+import { SiCsswizardry, SiJavascript, SiRedux } from 'react-icons/si'
+import { TiHtml5 } from 'react-icons/ti'
 import work from '../../styles/components/work.module.scss'
 
 const WorkPage = () => {
@@ -12,7 +15,7 @@ const WorkPage = () => {
       <section className={work.sections}>
         <Link href={'https://pokeapi-df.netlify.app'} target='_blank' className={work.shadow}>
           <Image
-          className={work.img}
+            className={work.img}
             src={'/../public/pokedex-pc.jpg'}
             width={600}
             height={400}
@@ -21,12 +24,21 @@ const WorkPage = () => {
         <section>
           <h2>Pokedex</h2>
           <p>This is a web app where you can find the pokemon that you want. It consumes the Pokemon's API with Axios library and has Redux in it. I made all the Front-End and the most difficult challenge was learn and apply Redux</p>
+          <div>
+            I used:
+          </div>
+          <div className={work.logos}>
+            <span> <FaReact /> </span>
+            <span> <FaSass /> </span>
+            <span> <SiJavascript /> </span>
+            <span> <SiRedux /> </span>
+          </div>
         </section>
       </section>
       <section className={work.sections}>
         <Link href={'https://alma-shuryah.netlify.app'} target='_blank' className={work.shadow}>
           <Image
-          className={work.img}
+            className={work.img}
             src={'/../public/alma-pc.jpg'}
             width={600}
             height={400}
@@ -35,12 +47,20 @@ const WorkPage = () => {
         <section>
           <h2>Alma Shuryah</h2>
           <p>This is a Landing Page that I made for a local client as a Freelancer. It was made from a scratch with HTML, CSS and JS. The most difficult challenge was the design part working with the client itself, I had to make about 5 designs until get with the definitive one.</p>
+          <div>
+            I used:
+          </div>
+          <div className={work.logos}>
+            <span> <TiHtml5 /> </span>
+            <span> <SiCsswizardry /> </span>
+            <span> <SiJavascript /> </span>
+          </div>
         </section>
       </section>
       <section className={work.sections}>
         <Link href={'https://rick-and-morty-app-df.netlify.app'} target='_blank' className={work.shadow}>
           <Image
-          className={work.img}
+            className={work.img}
             src={'/../public/rym-pc.jpg'}
             width={600}
             height={400}
@@ -49,12 +69,20 @@ const WorkPage = () => {
         <section>
           <h2>Rick and Morty's Character finder</h2>
           <p>This is an app that allows you to find every location and its characters in Rick and Morty's serie. It's made from a scratch with React and vanilla CSS. It consumes the Rick and Morty's API with Axios library. I made all the Front-End and the most difficult challenge was to make the controlled input in the search bar</p>
+          <div>
+            I used:
+          </div>
+          <div className={work.logos}>
+            <span> <FaReact /> </span>
+            <span> <SiCsswizardry /> </span>
+            <span> <SiJavascript /> </span>
+          </div>
         </section>
       </section>
       <section className={work.sections}>
         <Link href={'https://weather-app-df.netlify.app'} target='_blank' className={work.shadow}>
           <Image
-          className={work.img}
+            className={work.img}
             src={'/../public/weather-pc.jpg'}
             width={600}
             height={400}
@@ -63,12 +91,20 @@ const WorkPage = () => {
         <section>
           <h2>Weather App</h2>
           <p>This is a web app that gives you the weather information from your geolocation. I consumes the weather's API with Axios and it was made with React and vanilla CSS. I made all the Front-End and the most challenging part of the proccess was made the Loader page with the logic and the continuous animation because I made it while I was learning basic CSS</p>
+          <div>
+            I used:
+          </div>
+          <div className={work.logos}>
+            <span> <FaReact /> </span>
+            <span> <SiCsswizardry /> </span>
+            <span> <SiJavascript /> </span>
+          </div>
         </section>
       </section>
       <section className={work.sections}>
         <Link href={'https://crud-user-cards-df.netlify.app'} target='_blank' className={work.shadow}>
           <Image
-          className={work.img}
+            className={work.img}
             src={'/../public/crud-pc.jpg'}
             width={600}
             height={400}
@@ -77,6 +113,14 @@ const WorkPage = () => {
         <section>
           <h2>CRUD Users</h2>
           <p>This is a web app that allows you to get the first touch with the CRUD Persistent Storage creating some Users with basic information. That means that you can create an user, refresh the page and it won't be deleted. Even if you close the page and re-open it in the same device, the user that you made will still be there. I made all the Front-End and the most difficult challenge was the logic on the local storage function.</p>
+          <div>
+            I used:
+          </div>
+          <div className={work.logos}>
+            <span> <FaReact /> </span>
+            <span> <SiCsswizardry /> </span>
+            <span> <SiJavascript /> </span>
+          </div>
         </section>
       </section>
     </article>
